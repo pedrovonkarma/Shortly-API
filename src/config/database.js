7d9d8c26f94d7b79a07cad25da2657426ff210f0
Pedro <heeypedroo@gmail.com>
2023-03-02 23:58:40 -0300

    deploy ssl modification

--- src/config/database.js
+++ src/config/database.js
@@ -8,6 +8,6 @@ const configDatabase = {
   connectionString: process.env.DATABASE_URL,
 };
 
-if (process.env.MODE === "prod") configDatabase.ssl = true;
+if (true) configDatabase.ssl = true;
 
 export const db = new Pool(configDatabase);
